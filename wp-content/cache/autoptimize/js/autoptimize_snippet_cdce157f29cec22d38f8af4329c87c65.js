82a8b986baf149f2a259ae521d66507a5f6325cd
Mariia <maria.bookvoed@gmail.com>
2019-03-18 12:55:58 -0300

    add:db de servidor y wp-content

---
+++ wp-content/cache/autoptimize/js/autoptimize_snippet_cdce157f29cec22d38f8af4329c87c65.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('#menu-items li').children(this).addClass('sliding-link');$(".sliding-link").on('click',function(event){if(this.hash!==""){event.preventDefault();var hash=this.hash;$('html, body').animate({scrollTop:$(hash).offset().top},800,function(){window.location.hash=hash;});}});$("#reservar").on("click",function(){$("#form").toggleClass("display");});});
